2fef4607f5f91c495f150ff2149c1763ec129e65
kemaltt <ttnckemal@gmail.com>
2023-01-03 23:03:32 +0100

    comment deleted

--- backend/src/domain/User.js
+++ backend/src/domain/User.js
@@ -24,31 +24,3 @@ function makeUser({
 module.exports = {
   makeUser,
 };
-
-// function makeUser({
-//   _id,
-//   name,
-//   email,
-//   // profilePicture = "https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg",
-//   status = "off the line",
-//   passwordHash,
-//   passwordSalt,
-// }) {
-//   if (!email) {
-//     throw new Error("email must exist");
-//   }
-
-//   return {
-//     _id,
-//     name,
-//     // profilePicture,
-//     email,
-//     status,
-//     passwordHash,
-//     passwordSalt,
-//   };
-// }
-
-module.exports = {
-  makeUser,
-};
