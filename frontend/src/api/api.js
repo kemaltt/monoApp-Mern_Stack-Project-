50bc5a1bdfcffcac9b44a9d322aceebc6529abdc
kemaltt <ttnckemal@gmail.com>
2024-12-28 16:55:55 +0100

    Ändere die API-Basis-URL für die Produktionsumgebung

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
 export const apiBaseUrl =
   process.env.REACT_APP_API_PATH === "production"
-    ? "https://mono-v9zs.onrender.com"
+    ? "https://mono-app-mern-stack-project.vercel.app"
     : "http://localhost:9000";
